fcb52335d38998f1db9c49b2fb51b6c33d621aaf
yaohongweiisme <2558939179@qq.com>
2022-06-16 21:27:28 +0800

    分页查询员工时显示部门信息

--- src/main/java/com/wei/mybatisplus_pratice/controller/EmployeeController.java
+++ src/main/java/com/wei/mybatisplus_pratice/controller/EmployeeController.java
@@ -4,7 +4,9 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wei.mybatisplus_pratice.entity.Employee;
 import com.wei.mybatisplus_pratice.entity.EmployeeQuery;
+import com.wei.mybatisplus_pratice.service.DepartmentService;
 import com.wei.mybatisplus_pratice.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,26 +15,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-    public final EmployeeService employeeService;
-
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
+    @Autowired
+    public EmployeeService employeeService;
+    @Autowired
+    public DepartmentService departmentService;
     @PostMapping("/add")
     public String addEmployee(Employee employee){
         System.out.println("接收参数:"+employee);
         boolean save = employeeService.save(employee);
         return save?"添加成功!!!":"添加失败";
     }
+    /**
+     * 分页查询员工数据
+     * @param employeeQuery
+     * @return
+     */
     @GetMapping("/pageSearch")
     public Page<Employee> showPage(EmployeeQuery employeeQuery){
         QueryWrapper<Employee> wrapper = new QueryWrapper<>();
         wrapper.eq("dept_id",employeeQuery.getDept_id());
         int page=employeeQuery.getPage();
         int pageSize=employeeQuery.getPageSize();
-        Page<Employee> EmployeePage = new Page<>(page,pageSize);
-        EmployeePage= employeeService.page(EmployeePage,wrapper);
-        return EmployeePage;
+        Page<Employee> employeePage = new Page<>(page,pageSize);
+        employeePage= employeeService.page(employeePage,wrapper);
+        for (Employee record : employeePage.getRecords()) {
+            record.setDepartment(departmentService.getById(employeeQuery.getDept_id()));
+            }
+        return employeePage;
     }
 
 }
--- src/main/java/com/wei/mybatisplus_pratice/entity/EmployeeQuery.java
+++ src/main/java/com/wei/mybatisplus_pratice/entity/EmployeeQuery.java
@@ -1,8 +1,12 @@
 package com.wei.mybatisplus_pratice.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeQuery {
     int page;
     int pageSize;
